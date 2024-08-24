a3944e5ab4c8e316ad104a754e80b0852a9ad102
tamzid620 <tamzid620@gmail.com>
2024-08-24 12:46:14 +0600

    homeDescription zoom effect added

--- src/app/Components/HomeDescription/HomeDescription.jsx
+++ src/app/Components/HomeDescription/HomeDescription.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import img1 from "@/assests/icon/Architecture.png";
-import img2 from "@/assests/icon/UserExperience.png";
-import img3 from "@/assests/icon/StackMastery.png";
+import "./HomeDescription.css";
 import Image from "next/image";
 import { Nunito_Sans } from "next/font/google";
 import { Arsenal } from "next/font/google";
+import img1 from "@/assests/icon/Architecture.png";
+import img2 from "@/assests/icon/UserExperience.png";
+import img3 from "@/assests/icon/StackMastery.png";
 
 const nunito = Nunito_Sans({ subsets: ["latin"] });
 const arsenal = Arsenal({
-  weight: ["400" ,"700"],
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
 
 const HomeDescription = () => {
-
   const descriptionData = [
     {
       logo: img1,
@@ -30,7 +30,7 @@ const HomeDescription = () => {
     {
       logo: img3,
       title: "Full-Stack Mastery",
-        description:
+      description:
         "From crafting dynamic front-ends to building powerful back-end systems,I offer full-stack development services, ,Complete front-end to back-end solutions with efficient code",
     },
   ];
@@ -38,50 +38,15 @@ const HomeDescription = () => {
   return (
     <div className="pt-[150px] pb-14 bg-white flex justify-center text-center">
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm: grid-cols-1 gap-8">
-        {descriptionData.map(({logo, title, description}) => (
+        {descriptionData.map(({ logo, title, description }) => (
           <div key={title}>
             <span className="flex justify-center mb-3">
-              <Image className="w-[100px]" src={logo} alt="" />
+                <Image className="fiterdiscIcon zoom-in-out w-[100px]" src={logo} alt="" />
             </span>
             <h1 className={`${arsenal.className} mb-3 text-2xl `}>{title}</h1>
-            <p className={`${nunito.className} text-md`}>
-             {description}
-            </p>
+            <p className={`${nunito.className} text-md`}>{description}</p>
           </div>
         ))}
-        {/* <div>
-          <span className="flex justify-center">
-            <Image className="w-[80px]" src={img1} alt="" />
-          </span>
-          <h1>Scalable Architecture</h1>
-          <p>
-            Building robust and scalable web applications with a solid
-            architecture, utilizing the power of MongoDB, Express.js, React, and
-            Node.js to handle your growing business needs
-          </p>
-        </div>
-        <div>
-          <span className="flex justify-center">
-            <Image className="w-[80px]" src={img1} alt="" />
-          </span>
-          <h1>Scalable Architecture</h1>
-          <p>
-            Building robust and scalable web applications with a solid
-            architecture, utilizing the power of MongoDB, Express.js, React, and
-            Node.js to handle your growing business needs
-          </p>
-        </div>
-        <div>
-          <span className="flex justify-center">
-            <Image className="w-[80px]" src={img1} alt="" />
-          </span>
-          <h1>Scalable Architecture</h1>
-          <p>
-            Building robust and scalable web applications with a solid
-            architecture, utilizing the power of MongoDB, Express.js, React, and
-            Node.js to handle your growing business needs
-          </p>
-        </div> */}
       </div>
     </div>
   );
