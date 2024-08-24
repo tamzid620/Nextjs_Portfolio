6dc8ee18cf6e6fe390304c57b25cbbbc1eaa2aaa
tamzid620 <tamzid620@gmail.com>
2024-08-24 19:08:45 +0600

    homeLoading height fixed

--- src/app/Components/HomeLoading/HomeLoading.jsx
+++ src/app/Components/HomeLoading/HomeLoading.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomeLoading.css'
 
 const HomeLoading = () => {
+
+    const [expand, setExpand] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setExpand(true);
+        }, 3000); // 3 seconds delay
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className='htmlBody'>
-        <div class="loading-container">
-        <div class="loading-line" />
-    </div>
+            <div className={`loading-container ${expand ? 'expand' : ''}`}>
+                <div className="loading-line" />
+            </div>
         </div>
     );
 };
